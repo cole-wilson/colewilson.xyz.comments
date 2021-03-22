10b593449cb13bfbc45729fc58081b3f80cdef62
Cole Wilson <cole@colewilson.xyz>
2021-03-22 20:58:17 +0000

    added rate limiting

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, make_response, jsonify
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 from github import Github
 import os
+
+app = Flask(__name__)
+limiter = Limiter(
+	app,
+	key_func=get_remote_address,
+	default_limits=["2 per minute"],
+)
 
 g = Github(os.getenv("GITHUB_TOKEN"))
 
 app = Flask('app')
 
-@app.route('/comment', methods=['GET', 'POST'])
+@app.errorhandler(429)
+def ratelimit_handler(e):
+	return make_response("<code><h1>Error:</h1>You may only post once per hour. This is due to spam. I know you are very likely not a spam commenter or a robot, but it's neccesary to do this. If you would like to post again, either comment through GitHub or wait for an hour.<br>Thanks!<br><br>- Cole</code>",429)
+
+@limiter.request_filter
+def ip_whitelist():
+	print(request.remote_addr)
+	return request.remote_addr == os.getenv("my_ip") or  request.remote_addr == os.getenv("my_ip2") or  request.remote_addr == os.getenv("my_ip3")
+
+@app.route('/comment', methods=['GET'])
+@limiter.limit("1/hour")
+def no_comment():
+	return "<code>you must come here from a post! head over to <a href='//colewilson.xyz'>colewilson.xyz</a> to view one.</code>"
+
+@app.route('/comment', methods=['POST'])
+@limiter.limit("1/hour")
 def create_comment():
 	ititle = request.form['url']
 	body = request.form['body']
@@ -21,6 +45,5 @@ def create_comment():
 	issue.create_comment(body)
 	return redirect(f"https://colewilson.xyz/{ititle}#comments", code=302)
 
-# Bind to PORT if defined, otherwise default to 5000.
 port = int(os.environ.get('PORT', 5000))
 app.run(host='0.0.0.0', port=port)
