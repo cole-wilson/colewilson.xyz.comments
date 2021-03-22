1756cd3f774f5cb5c836e0784cc72cabff4d02fb
Cole Wilson <cole@colewilson.xyz>
2021-03-22 14:17:06 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 limiter = Limiter(
 	app,
 	key_func=get_remote_address,
-	default_limits=["2 per minute"],
+	default_limits=["1000 per minute"],
 )
 
 g = Github(os.getenv("GITHUB_TOKEN"))
